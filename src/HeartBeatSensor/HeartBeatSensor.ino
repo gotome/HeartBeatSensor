bb690898faf57b3e757e8a9a7bfd2e4721202d40
gotome <g.riess@gmx.at>
2021-09-24 22:06:31 +0200

    add ECG and play tone if heart rate drops < 60bpm

--- src/HeartBeatSensor/HeartBeatSensor.ino
+++ src/HeartBeatSensor/HeartBeatSensor.ino
@@ -1,117 +1,107 @@
-//****************************************
+///////////////////////////////////////////
 //                LIBRARYS
-//****************************************
-//general
+///////////////////////////////////////////
+
+// G E N E R A L 
 #include <stdlib.h>
-//arduino
+// A R D U I N O
 #include <Arduino.h>
 #include <math.h>
-//display
+// D I S P L A Y
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-//project
+// H E A R T  R A T E  S E N S O R
+#include <DFRobot_Heartrate.h>
+// P R O J E C T
 #include "HeartBeatSensor.h"
  
-//****************************************
-//                TABLES
-//****************************************
-
-
-//****************************************
+///////////////////////////////////////////
 //                GLOBALS
-//****************************************
+///////////////////////////////////////////
 const int i16BuzzerPin = DI_PIN_9;
-
-// We'll set up an array with the notes we want to play
-// change these values to make different songs!
-// Length must equal the total number of notes and spaces 
-const int i16SongLength = 18;
-
-// Notes is an array of text characters corresponding to the notes
-// in your song. A space represents a rest (no tone)
-
-//char notes[] = "cdfda ag cdfdg gf "; // a space represents a rest
-char u8NotesArr[] = "C C C C "; // a space represents a rest
-
-// Beats is an array values for each note and rest.
-// A "1" represents a quarter-note, 2 a half-note, etc.
-// Don't forget that the rests (spaces) need a length as well.
-
-//int beats[] = {1,1,1,1,1,1,4,4,2,1,1,1,1,1,1,4,4,2};
-int i16BeatsArr[] = {2,1,2,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1};
-
-// The tempo is how fast to play the song.
-// To make the song play faster, decrease this value.
-int i16Tempo = 150;
 
 // calculated heart rate from heart rate sensor
 int i16HeartRate = 0;
 
-//raw heart rate analog value
+// raw heart rate analog value
 int i16SensorVal = 0;
 
+// coordinates for electro cardiogram
+int i16OldX = 0; 
+int i16X = 0; 
+int i16OldY = 0; 
+int i16Y = 0; 
 
-//****************************************
+// tone timer (milliseconds)
+long l32PrevToneTime=0;
+long l32CurrToneTime=0;
+
+// time until the next tone is played 
+const long l32EventIntervall = 1000;
+
+// the tempo is how fast to play the song.
+// to make the song play faster, decrease this value.
+const long l32ToneDuration = 150;
+
+// Frequency of the alarm tone
+const int i16ToneFrequency = 1000;
+
+///////////////////////////////////////////
 //         FUNCTION DECLARATIONS
-//****************************************
+///////////////////////////////////////////
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+DFRobot_Heartrate heartrate(DIGITAL_MODE); ///< ANALOG_MODE or DIGITAL_MODE
 
-// //****************************************
-// //         FUNCTION DEFINITIONS
-// //****************************************
+///////////////////////////////////////////
+//         FUNCTION DEFINITIONS
+///////////////////////////////////////////
+
 void playTone(void) {
-  int i, duration;
-  
-  for (i = 0; i < i16SongLength; i++) // step through the song arrays
-  {
-    duration = i16BeatsArr[i] * i16Tempo;  // length of note/rest in ms
-    
-    if (u8NotesArr[i] == ' ')          // is this a rest? 
-    {
-      delay(duration);            // then pause for a moment
-    }
-    else                          // otherwise, play the note
-    {
-      tone(i16BuzzerPin, frequency(u8NotesArr[i]), duration);
-      delay(duration);            // wait for tone to finish
-    }
-    delay(i16Tempo/10);              // brief pause between notes
+  // play tone each second
+  l32CurrToneTime = millis();
+
+  if ((l32CurrToneTime - l32PrevToneTime) >= l32EventIntervall) {
+    tone(i16BuzzerPin, i16ToneFrequency, l32ToneDuration);
+    l32PrevToneTime = l32CurrToneTime;
   }
 }
 
-int frequency(char u8Note) {
-  // This function takes a note character (a-g), and returns the
-  // corresponding frequency in Hz for the tone() function.
-  
-  int i16Idx;
-  const int i16NumNotes = 8;  // number of notes we're storing  
-  // The following arrays hold the note characters and their
-  // corresponding frequencies. The last "C" note is uppercase
-  // to separate it from the first lowercase "c". If you want to
-  // add more notes, you'll need to use unique characters.
 
-  // For the "char" (character) type, we put single characters
-  // in single quotes.
+void showStats(int heartRate, int i16SensorVal) 
+{ 
+  char charVal[5]; 
+  dtostrf(heartRate, 4, 0, charVal);
 
-  char u8NamesArr[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
-  const int i16FrequenciesArr[] = {262, 294, 330, 349, 392, 440, 494, 523};
-  
-  // Now we'll search through the letters in the array, and if
-  // we find it, we'll return the frequency for that note.
-  
-  for (i16Idx = 0; i16Idx < i16NumNotes; i16Idx++)  // Step through the notes
-  {
-    if (u8NamesArr[i16Idx] == u8Note)         // Is this the one?
-    {
-      return(i16FrequenciesArr[i16Idx]);     // Yes! Return the frequency
-    }
+  //reset cursor if end of display is reached
+  if (i16X > 127) {
+    display.clearDisplay(); 
+    i16X = 0;
+    i16OldX = i16X; 
   }
-  
-  return(0);  // We looked through everything and didn't find it,
-              // but we still need to return a value, so return 0.
+
+  i16Y = 60-(i16SensorVal/16);
+  i16OldX = i16X; 
+  i16OldY = i16Y; 
+
+  //show cardiogramm
+  display.writeLine(i16OldX, i16OldY, i16X, i16Y, WHITE);
+
+  //show beats per minute
+  display.writeFillRect(0,50,128,16,BLACK);
+  display.setCursor(10, 50);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.print(charVal);
+  display.print(" BPM");
+  display.display();
+
+  //finally increase ECG x-coordinate
+  i16X++;
 }
 
-
+///////////////////////////////////////////
+//              S E T U P
+//////////////////////////////////////////
 void setup() 
 {
   //begin serial communication
@@ -122,52 +112,28 @@ void setup()
   } 
   display.clearDisplay();
   //pin mode setup 
-  pinMode(8, OUTPUT);
+  pinMode(i16BuzzerPin, OUTPUT);
   //delay time
   delay(10);
-  pinMode(i16BuzzerPin, OUTPUT);
 }
 
-void showStats(int heartRate) 
-{ 
-  char charVal[5]; 
-  display.clearDisplay();   
-  dtostrf(heartRate, 4, 0, charVal);
-
-  //show cardiogramm
-  //display.writeLine();
-  display.drawLine(0, 0, 10, 10, WHITE); 
-
-  //show beats per minute
-  display.writeFillRect(0,50,128,16,BLACK);
-  display.setCursor(10, 35);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.print("Heart Rate");  
-  display.setCursor(30, 50);
-  display.setTextSize(2);
-  display.print(charVal);
-  display.setTextSize(1);
-  display.print("BPM");
-
-  display.display();
-}
-
-
+///////////////////////////////////////////
+//              M A I N
+//////////////////////////////////////////
 void loop() 
-{
-  //show statistics on lcd terminal  
-  showStats(60);
+{  
+  i16SensorVal = 700; //heartrate.getValue(A0);
+  i16HeartRate = 60; //heartrate.getRate();
+  //Serial output
+  Serial.print("heart rate: " + String(i16HeartRate) + "\t");
+  Serial.println("sensor value: " + String(i16SensorVal) + "\t");
+  Serial.println("x-coordinate: " + String(i16X) + "\t");
 
-  //alarm if heart rate is to low
+  //show statistics on lcd terminal  
+  showStats(i16HeartRate, i16SensorVal);
+
+  //play alarm if heart rate is to low
   if (i16HeartRate < 60) {
     playTone();
-  }
-  
-  delay(10);
-
-  // We only want to play the song once, so we'll pause forever:
-  while(true){}
-  // If you'd like your song to play over and over,
-  // remove the above statement
+  }  
 }
